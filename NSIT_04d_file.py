f2f8060abe7201bfc7f801b6906af73cf0781afb
FwaGras <124463406+FwaGras@users.noreply.github.com>
2023-11-23 17:28:46 +0100

    Add files via upload

--- NSIT_04d_file.py
+++ NSIT_04d_file.py
@@ -1,32 +1,3 @@
-class PileVide(Exception):
-    """
-    extraction à partir d'une pile vide
-    """
-    pass
-
-class Pile:
-    def __init__(self):
-        self.memoire = []
-        self.taille = 0
-    def __repr__(self):
-        mem =""
-        for i in range(len(self.memoire)):
-            mem += self.memoire[i] 
-        return mem
-    def estVide(self):
-        return self.taille == 0
-    def ajoute(self,x):
-        self.memoire.append(x)
-        self.taille += 1
-        return None
-    def extrait(self):
-        if self.taille == 0:
-            raise PileVide
-        else:
-            x = self.memoire.pop()
-            self.taille -= 1
-            return x
-
 class FileVide(Exception):
     """
         extraction à partir d'une file vide
@@ -35,21 +6,23 @@ class FileVide(Exception):
 
 class File:
     def __init__(self):
-        self.pA = Pile()
-        self.pB = Pile()
-        self.taille = 0
+        self.L = []
+    
     def estVide(self):
-        return self.taille == 0
+        return self.L == []
+    
     def ajoute(self,x):
-        self.pA.ajoute(x)
-        self.taille += 1
-        return None
+        self.L.append(x)
+        
     def extrait(self):
-        if self.taille == 0:
-            raise FileVide
-        elif self.pB.estVide():
-            while not self.pA.estVide():
-                self.pB.ajoute(self.pA.extrait())
-        x = self.pB.extrait() # ce n'est pas une erreur d'indentation !
-        self.taille -= 1
-        return x
+        assert not self.estVide(), "file vide"
+        return self.L.pop(0)
+    
+    def taille(self):
+        return len(self.L)
+    
+    def sommet(self):
+        return self.L[0]
+    
+    def present(self, x):
+        return x in self.L
--- recherche_bfs.py
+++ recherche_bfs.py
@@ -3,7 +3,7 @@
 
 #importation des bibliothèques
 import networkx as nx
-from NSIT_O4d_file import File
+from NSIT_04d_file import *
 
 def chercher_bfs(laby:nx.Graph, source:int = None, destination:int = None)->list:
     """ Cherche le chemin le plus court entre les sommets source et destination
@@ -19,8 +19,17 @@ def chercher_bfs(laby:nx.Graph, source:int = None, destination:int = None)->list
     if destination == None:   
         destination = nodes[-1]
     
+    #itinéraire = [] #amélioration 1 : renvoyé la liste des arêtes parcourues pour trouver la sortie
     sommet_visite = []
     f = File()
-    
-        
-    return #nx.shortest_path(laby, source, destination)
+    f.ajoute(source)
+    while f.estVide() != True:
+        tmp = f.extrait()
+        print(tmp)
+        if tmp not in sommet_visite:
+            sommet_visite.append(tmp)
+        for voisin in laby.neighbors(tmp):
+            if voisin not in sommet_visite and not f.present(voisin):
+                f.ajoute(voisin)
+
+    return sommet_visite #nx.shortest_path(laby, source, destination)
